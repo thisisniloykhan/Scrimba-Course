682c2f077a23a59dc0d52fda51c0f998e07c8145
Niloy Khan <niloykhan.dev@gmail.com>
2026-06-24 16:52:08 +0600

    learning POST request

--- 11. Node.js/02. Build a Fullstack Node App/14. Explainer- Adding POST/handlers/routeHandlers.js
+++ 11. Node.js/02. Build a Fullstack Node App/14. Explainer- Adding POST/handlers/routeHandlers.js
@@ -1,13 +1,16 @@
-import { getData } from '../utils/getData.js'
-import { sendResponse } from '../utils/sendResponse.js'
+import { getData } from "../utils/getData.js";
+import { sendResponse } from "../utils/sendResponse.js";
 
 export async function handleGet(res) {
-  const data = await getData()
-  const content = JSON.stringify(data)
-  sendResponse(res, 200, 'application/json', content)
-}  
+  const data = await getData();
+  const content = JSON.stringify(data);
+  sendResponse(res, 200, "application/json", content);
+}
 
 // handlePost
+export async function handlePost(req, res) {
+  console.log("POST request received");
+}
 
 /*
 Challenge:
--- 11. Node.js/02. Build a Fullstack Node App/14. Explainer- Adding POST/server.js
+++ 11. Node.js/02. Build a Fullstack Node App/14. Explainer- Adding POST/server.js
@@ -1,26 +1,26 @@
-import http from 'node:http'
-import { serveStatic } from './utils/serveStatic.js'
-import { handleGet } from './handlers/routeHandlers.js'
+import http from "node:http";
+import { serveStatic } from "./utils/serveStatic.js";
+import { handleGet, handlePost } from "./handlers/routeHandlers.js";
 
-const PORT = 8000
+const PORT = 8000;
 
-const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-
-    if (req.url === '/api') {
-        if (req.method === 'GET') {
-            return await handleGet(res)
-        }
-/*
+  if (req.url === "/api") {
+    if (req.method === "GET") {
+      return await handleGet(res);
+    } else if (req.method === "POST") {
+      return await handlePost(req, res);
+    }
+    /*
 Challenge: 
    1. Add a route for a POST request to '/api'.
    2. When a request comes in, pass the req and res to handlePost().
 */
-    }
-    else if (!req.url.startsWith('/api')) {
-        return await serveStatic(req, res, __dirname)
-    }
-})
+  } else if (!req.url.startsWith("/api")) {
+    return await serveStatic(req, res, __dirname);
+  }
+});
 
-server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
