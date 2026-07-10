5e920231ef88139806c59e2001ccf341c8629a61
Niloy Khan <niloykhan.dev@gmail.com>
2026-07-10 22:17:54 +0600

    Started React Project

--- 15. React.js Fundamentals/01. Static Pages/01. First React Code/index.jsx
+++ 15. React.js Fundamentals/01. Static Pages/01. First React Code/index.jsx
@@ -2,3 +2,8 @@
 // You can render anything you want to the screen, doesn't have
 // to be an h1 element.
 
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<h1>Hello, React</h1>);
--- 15. React.js Fundamentals/01. Static Pages/02. First React Challenge/index.jsx
+++ 15. React.js Fundamentals/01. Static Pages/02. First React Challenge/index.jsx
@@ -3,3 +3,18 @@
  * This time, try rendering an unordered list with 2-3 list items inside
  * with why you're excited to be learning React.
  */
+
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <ul>
+    <li>React makes the code more easier to manage</li>
+    <li>It's easier to copy the code multiple times</li>
+    <li>
+      React is a popular frontend library so it is a great tool for career
+      development
+    </li>
+  </ul>,
+);
